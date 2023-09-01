eeac97a2dd5b4dd9947a8bb2e886e18007f95eec
DebbiePham <ppham060789@gmail.com>
2023-09-01 12:56:44 -0700

    updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,13 +7,9 @@ const Form = (props) => {
 
     const [input, setInput] = useState("");
 
-    const onChange = (e) => {
-        e.preventDefault();
-        
-        setInput(e.target.value);
-    }
+    
 
-    const onSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
 
         let item = {
@@ -22,14 +18,14 @@ const Form = (props) => {
         };
 
         setTask([...task, item]);
-        e.target.value = "";
+        setInput("");
     }
 
 
     return (
         <div>
-            <form onSubmit = {onSubmit} className='mb-5 d-flex'>
-                <input className='m-2 form-control' type='text' value={input} onChange={onChange}></input>
+            <form onSubmit = {handleSubmit} className='mb-5 d-flex'>
+                <input className='m-2 form-control' type='text' value={input} onChange={(e) => { setInput(e.target.value); }} placeholder='Enter task' />
                 <button className='m-2 btn btn-success rounded'>Add</button>
             </form>
         </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -17,9 +17,9 @@ const Todo = (props) => {
 
     return (
         <>
-            <div className='d-flex'>
-                <div>
-                    <p><strong>{item.name}</strong></p>
+            <div>
+                <div className='d-flex align-items-center'>
+                    <h4 className='fw-bold m-3'>{item.name}</h4>
                     <label style={{ textDecorationLine: item.isCompleted ? "line-through":"none"}} htmlFor='checkbox'>Completed?</label>
                     <input onChange={onChange} type='checkbox' name='' checked={item.isCompleted} className='m-2'></input>
                     <button onClick={onClick} className='m-2 btn btn-danger'> Delete </button>
