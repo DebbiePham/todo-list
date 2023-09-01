fd79bbea8c87c83ff775c9545b930a2ea92248bf
DebbiePham <ppham060789@gmail.com>
2023-09-01 13:12:59 -0700

    fixed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,13 +4,13 @@ import React, {useState} from 'react';
 const Todo = (props) => {
     const {item, index, task, setTask} = props;
 
-    const onClick = () => {
+    const handleClick = () => {
         const newList = () => task.filter((item) => task.indexOf(item) !== index);
 
         setTask(newList);
     };
 
-    const onChange = () => {
+    const handleChange = () => {
         task[index].isCompleted = !task[index].isCompleted;
         setTask([...task]);
     };
@@ -21,8 +21,8 @@ const Todo = (props) => {
                 <div className='d-flex align-items-center'>
                     <h4 className='fw-bold m-3'>{item.name}</h4>
                     <label style={{ textDecorationLine: item.isCompleted ? "line-through":"none"}} htmlFor='checkbox'>Completed?</label>
-                    <input onChange={onChange} type='checkbox' name='' checked={item.isCompleted} className='m-2'></input>
-                    <button onClick={onClick} className='m-2 btn btn-danger'> Delete </button>
+                    <input onChange={handleChange} type='checkbox' name='' checked={item.isCompleted} className='m-2'></input>
+                    <button onClick={handleClick} className='m-2 btn btn-danger'> Delete </button>
                 </div>
             </div>
         </>
